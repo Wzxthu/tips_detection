51efb07b6dc5762dc0019c69581233410ccc545b
Wzxthu <wangzx13@mails.tsinghua.edu.cn>
2016-08-19 10:12:21 -0400

    Add files via upload

--- keras/CNN+PCA/6.tip_find.py
+++ keras/CNN+PCA/6.tip_find.py
@@ -1,20 +1,22 @@
 
 # coding: utf-8
 
-## find tips from middle of needles
+## find tips from mids along low intensty
 import numpy as np
 import nrrd
 
 Case_num = 64
+# threshold of searching ability
 threshold = 15
 
 nrrdData = nrrd.read('Case0%d.nrrd'%Case_num)
 im = nrrdData[0]
 
+# load mid information
 mid = np.load(str(Case_num)+'-mid.save')
 print(mid.shape)
 
-## search upper layer
+# search region in upper layer
 def pick(coord):
     if int(coord[2]) >= 100:
         return 0
@@ -31,7 +33,7 @@ for coord in mid:
         flag = 0
         temp_coord = [int(coord[0]),int(coord[1]),int(coord[2])]
         temp = im[int(coord[0]),int(coord[1]),int(coord[2])]
-        # search in a circle
+        # find lowest intensity in a circle
         while temp != 0:
             for x in range(5):
                 for y in range(5):
@@ -42,7 +44,7 @@ for coord in mid:
                 coord = temp_coord
                 break
             coord = temp_coord
-        # up layer
+        # up layer searching
         if pick([int(coord[0]),int(coord[1]),int(coord[2]+1)]):
             flag = 1
             coord[2] += 1
@@ -52,6 +54,13 @@ mid = update
 print(np.shape(mid))
 print(mid)
 
+# save mask of final tips
+mask = np.zeros(im.shape)
+for coord in mid:
+    mask[int(coord[0]),int(coord[1]),int(coord[2])]=1.0
+nrrd.write('mask-tips-%d.nrrd'%Case_num, mask, nrrdData[1])
+
+# save final tips' coords
 f = open(str(Case_num)+'-tips.save', 'wb')
 np.save(f, mid)
 f.close
